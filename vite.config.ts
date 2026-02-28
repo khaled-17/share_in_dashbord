cd543e76394c56a8074e69e3b2aa58886a2787fd
ram <ram@example.com>
2026-02-28 17:03:42 +0200

    ddds

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react'
 import pkg from './package.json'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-
+  // تأكد من وجود هذا السطر
+  base: '/',
   // 👇 سيبها زي ما هي
   // 👇 Use '/' by default (for Vercel and local) and '/share_in_dashbord/' for GitHub Pages
-  base: (command === 'serve' || process.env.VERCEL) ? '/' : '/share_in_dashbord/',
+  // base: (command === 'serve' || process.env.VERCEL) ? '/' : '/share_in_dashbord/',
 
   // 👇 الإضافة الجديدة
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
-}))
+})
